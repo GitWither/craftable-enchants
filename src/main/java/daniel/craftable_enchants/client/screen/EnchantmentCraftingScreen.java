44fb95454c4090c2c0dbc524f7ec0f1d4a3bf6c7
GitWither <wither.tweets@gmail.com>
2021-10-20 22:09:22 +0200

    Fixed bug with random text characters and disappearing texture

--- src/main/java/daniel/craftable_enchants/client/screen/EnchantmentCraftingScreen.java
+++ src/main/java/daniel/craftable_enchants/client/screen/EnchantmentCraftingScreen.java
@@ -12,6 +12,7 @@ import net.minecraft.enchantment.EnchantmentLevelEntry;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.registry.Registry;
@@ -20,6 +21,7 @@ import java.util.List;
 
 public class EnchantmentCraftingScreen extends HandledScreen<ScreenHandler> {
     private static final Identifier TEXTURE = new Identifier(CraftableEnchants.MOD_ID, "textures/gui/container/enchantment_crafting_table.png");
+    private static final int WHITE = (255 << 16) + (255 << 8) + 255;
 
     List<EnchantmentLevelEntry> enchantments;
     private int maxEnchants;
@@ -42,20 +44,33 @@ public class EnchantmentCraftingScreen extends HandledScreen<ScreenHandler> {
         RenderSystem.setShader(GameRenderer::getPositionTexShader);
         RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
         RenderSystem.setShaderTexture(0, TEXTURE);
+
         int x = (width - backgroundWidth) / 2;
         int y = (height - backgroundHeight) / 2;
         drawTexture(matrices, x, y, 0, 0, backgroundWidth, backgroundHeight);
 
-        for (int i = firstEnchantment, yDelta = 0; i <  firstEnchantment + 3; i++, yDelta++) {
-            Enchantment currentEnchantment = Registry.ENCHANTMENT.get(i);
+        if (handler.slots.get(0).hasStack()) {
+            for (int i = firstEnchantment, yDelta = 0; i <  firstEnchantment + 3; i++, yDelta++) {
+                Enchantment currentEnchantment = Registry.ENCHANTMENT.get(i);
 
-            if (currentEnchantment != null) {
-                drawTexture(matrices, x + 60, y + yDelta * 19 + 16, 0, 166, 95, 19);
-                this.textRenderer.drawWithShadow(matrices, currentEnchantment.getName(1),x + 62, y + yDelta * 19 + 20, 3419941);
+                if (currentEnchantment != null) {
+                    final int buttonX = x + 44;
+                    final int buttonY = y + yDelta * 19 + 16;
+
+                    //offset the starting position of the mouse
+                    final int mouseZoneX = mouseX - buttonX;
+                    final int mouseZoneY = mouseY - buttonY;
+
+                    boolean isCursorOnButton = (mouseZoneX >= 0 && mouseZoneY >= 0) && (mouseZoneX <= 110 && mouseZoneY <= 19);
+
+                    RenderSystem.setShaderTexture(0, TEXTURE);
+
+                    drawTexture(matrices, buttonX, buttonY, 0, isCursorOnButton ? 204 : 166, 110, 19);
+
+                    this.textRenderer.drawWithShadow(matrices, new TranslatableText(currentEnchantment.getTranslationKey()),buttonX + 2, buttonY + 4, (255 << 16) + (255 << 8) + 255);
+                }
             }
         }
-
-        //this.textRenderer.draw(matrices, "fucking kys", 155, 90, 3419941);
     }
 
     @Override
@@ -83,6 +98,6 @@ public class EnchantmentCraftingScreen extends HandledScreen<ScreenHandler> {
         super.init();
         // Center the title
         titleX = (backgroundWidth - textRenderer.getWidth(title)) / 2;
-        titleY -= 2;
+        titleY -= 1;
     }
 }
