c94574bb39f709eb428b6d5ddf188939fb4589ba
GitWither <wither.tweets@gmail.com>
2021-10-26 21:41:17 +0200

    Added model, blockstates to table. Crafted books can no longer be fragmented.

--- src/main/java/daniel/craftable_enchants/block/EnchantmentCraftingTableBlock.java
+++ src/main/java/daniel/craftable_enchants/block/EnchantmentCraftingTableBlock.java
@@ -3,26 +3,43 @@ package daniel.craftable_enchants.block;
 import daniel.craftable_enchants.screen.EnchantmentCraftingScreenHandler;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.HorizontalFacingBlock;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.screen.CraftingScreenHandler;
+import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.screen.NamedScreenHandlerFactory;
 import net.minecraft.screen.ScreenHandlerContext;
 import net.minecraft.screen.SimpleNamedScreenHandlerFactory;
 import net.minecraft.stat.Stats;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.DirectionProperty;
 import net.minecraft.text.Text;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
 public class EnchantmentCraftingTableBlock extends Block {
+    public static final DirectionProperty FACING = HorizontalFacingBlock.FACING;
     public static final Text TITLE = new TranslatableText("container.enchantment_crafting");
 
     public EnchantmentCraftingTableBlock(Settings settings) {
         super(settings);
+        this.setDefaultState(this.getDefaultState().with(FACING, Direction.NORTH));
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+
+    @Nullable
+    @Override
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        return this.getDefaultState().with(FACING, ctx.getPlayerFacing().getOpposite());
     }
 
     @Override
