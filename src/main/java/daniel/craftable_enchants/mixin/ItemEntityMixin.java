c45c6d36ecdc97c47467510d211c3a340e88288b
GitWither <wither.tweets@gmail.com>
2021-11-29 23:06:47 +0100

    A fragment will now spawn for each enchantment from a book (Influenced by feedback)

--- src/main/java/daniel/craftable_enchants/mixin/ItemEntityMixin.java
+++ src/main/java/daniel/craftable_enchants/mixin/ItemEntityMixin.java
@@ -2,11 +2,15 @@ package daniel.craftable_enchants.mixin;
 
 import daniel.craftable_enchants.CraftableEnchants;
 import daniel.craftable_enchants.item.EnchantmentFragmentItem;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.EnchantmentLevelEntry;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.mob.CreeperEntity;
+import net.minecraft.item.EnchantedBookItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.nbt.NbtCompound;
@@ -17,6 +21,8 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Map;
 
 @Mixin(ItemEntity.class)
 public abstract class ItemEntityMixin extends Entity {
@@ -32,14 +38,19 @@ public abstract class ItemEntityMixin extends Entity {
         if (!this.getStack().isEmpty() && this.getStack().isOf(Items.ENCHANTED_BOOK) && source.getSource() instanceof CreeperEntity) {
             NbtCompound bookNbt = this.getStack().getNbt();
             if (bookNbt != null) {
-                if (!this.getStack().getNbt().contains(EnchantmentFragmentItem.FROM_FRAGMENT_KEY)) {
-                    ItemStack stack = CraftableEnchants.ENCHANTMENT_FRAGMENT.getDefaultStack();
-                    NbtCompound enchant = this.getStack().getNbt();
-                    if (enchant != null) {
-                        enchant.putByte("Uses", (byte)RandomUtils.nextInt(4, 9));
-                        stack.setNbt(enchant.copy());
+                if (!bookNbt.contains(EnchantmentFragmentItem.FROM_FRAGMENT_KEY)) {
+                    //note to future self: this only works on enchanted books if they store enchants in the StoredEnchantments NBT tag
+                    Map<Enchantment, Integer> enchantments = EnchantmentHelper.get(this.getStack());
+
+                    for (Enchantment enchantment : enchantments.keySet()) {
+                        ItemStack stack = CraftableEnchants.ENCHANTMENT_FRAGMENT.getDefaultStack();
+                        NbtCompound enchant = stack.getOrCreateNbt();
+                        if (enchant != null) {
+                            enchant.putByte("Uses", (byte)RandomUtils.nextInt(4, 9));
+                        }
+                        EnchantedBookItem.addEnchantment(stack, new EnchantmentLevelEntry(enchantment, enchantments.get(enchantment)));
+                        ItemEntity entity = this.dropStack(stack);
                     }
-                    ItemEntity entity = this.dropStack(stack);
                 }
             }
         }
